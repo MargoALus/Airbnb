4e625c93107886f43267dd2029271b3951bb25d0
Margo Lusinchi <margolusinchi@Margos-MacBook-Pro-2.local>
2019-07-10 01:35:52 +0100

    first

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  
+  buttonClicked: Boolean = false;
+
+  constructor(private navCtrl: NavController) {}
+
+  isButtonClicked() {
+    this.buttonClicked = !this.buttonClicked;
+    console.log(this.buttonClicked);
+  }
+
+  navigateToProfile() {
+    this.navCtrl.navigateForward("profile");
+  }
+
+}
+
